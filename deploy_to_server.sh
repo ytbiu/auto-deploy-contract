68ad73e75ab1026762c0e606d10ac8a0d073aa36
ytbiu <174920143@qq.com>
2025-04-24 15:06:49 +0800

    add go mod gitignore

--- deploy_to_server.sh
+++ deploy_to_server.sh
@@ -10,15 +10,23 @@ GIT_BRANCH=${1:-"main"}
 
 # 在服务器上拉取最新代码并部署
 echo "连接服务器并部署..."
-ssh $SERVER_USER@$SERVER_IP "cd $SERVER_DIR && \
-    git fetch && \
-    git checkout $GIT_BRANCH && \
-    git pull origin $GIT_BRANCH && \
-    rm -rf ./build && \
-    mkdir -p ./build && \
-    go build -o ./build/auto-deploy-contract ./main.go && \
-    cp .env ./build/ && \
-    cd build && \
-    nohup ./auto-deploy-contract > output.log 2>&1 &"
+ssh $SERVER_USER@$SERVER_IP << 'EOF'
+cd /data/xaa/auto-deploy-contract
+pid=$(pgrep auto-deploy-contract)
+if [ ! -z "$pid" ]; then
+    echo "终止已运行的程序 (PID: $pid)..."
+    kill $pid
+    sleep 2
+fi
+git fetch
+git checkout main
+git pull origin main
+rm -rf ./build
+mkdir -p ./build
+go build -o ./build/auto-deploy-contract ./main.go
+cp .env ./build/
+cd build
+nohup ./auto-deploy-contract > output.log 2>&1 &
+EOF
 
 echo "部署完成！"
